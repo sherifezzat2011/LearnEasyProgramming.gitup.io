3039b3ca0fd6b1116d6c7de95ae173897c461221
sherifezzat2011 <sherif.ezzat2011@gmail.com>
2024-08-27 06:01:37 +0300

    MyWebSite
    Let'sGo

---
+++ EasyLearnPublisg/EasyLearn/Controllers/CoursesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyLearn.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Front()
+        {
+            return View();
+        }
+        public IActionResult Back()
+        {
+            return View();
+        }
+    }
+}
